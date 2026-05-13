85d578a3533175728d686b669680addc76e5827b
alexparavi <alexparavi@gmail.com>
2026-05-13 21:06:24 +0400

    refactor: simplyfi findSgort using min() and array map

--- Shortest-Word/shortest-word.php
+++ Shortest-Word/shortest-word.php
@@ -1,14 +1,10 @@
 <?php
 
-function findShort(string $string)
+function findShort(string $string): int
 {
     $words = explode(" ", $string);
     $length = array_map('strlen', $words);
-    $minLength = min($length);
-    $result = array_filter($words, function ($words) use ($minLength) {
-        return strlen($words) === $minLength;
-    });
-    return implode(" ", array_values($result));
+    return min($length);
 }
 
 var_dump(findShort("Let's travel abroad shall we we"));
